6bf2d15dafa6aa71f86e0d2e71d09bffcee76b94
Proviant <xxTrillionerxx@gmail.com>
2025-02-09 14:35:27 +0100

    Changed branch

--- java/src/main/java/general/Function.java
+++
@@ -1,7 +0,0 @@
-package general;
-
-public class Function<T> {
-    public int f(T element1, T element2) {
-        return element1.hashCode() - element2.hashCode();
-    }
-}
--- java/src/main/java/modules/settings/ConstSettingsModule.java
+++
@@ -1,7 +0,0 @@
-package modules.settings;
-
-public interface ConstSettingsModule
-{
-    String FXML_SETTINGS_WINDOW = "settings.fxml";
-    String FXML_SETTINGS_CSS    = "settings.css";
-}
