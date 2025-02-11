95783fb5732fa3c3c4be46572bfec36d0e9cf804
gi2000 <gi831990@gmail.com>
2025-02-11 21:33:06 +0100

    initial coding
    - updated .gitignore
    - added icons
    - added first button functions
    - updated dark_theme.tres

---
+++ scripts/menus/StartMenu.cs
@@ -0,0 +1,18 @@
+using Godot;
+
+namespace Goetterblick.scripts.menus;
+
+public partial class StartMenu : Control
+{
+	private string _pathToSettingsMenu = "res://scenes/settings.tscn";
+
+	private void OnQuitButtonUp()
+	{
+		GetTree().Quit();
+	}
+
+	private void OnSettingsButtonUp()
+	{
+		GetTree().ChangeSceneToFile(_pathToSettingsMenu);
+	}
+}
