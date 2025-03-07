8d009a5961eae054567f02594500d7fe5396a187
gi2000 <gi831990@gmail.com>
2025-03-07 16:03:14 +0100

    Expanded search functionality

--- scripts/general/node/SearchBar.cs
+++ scripts/general/node/SearchBar.cs
@@ -1,24 +1,50 @@
-using System;
 using Godot;
+using Godot.Collections;
+using Goetterblick.scripts.general.simple_objects;
+using static System.Enum;
 
 namespace Goetterblick.scripts.general.node;
 
 public partial class SearchBar : LineEdit
 {
-    [Export] private NodePath _pathToSearchContainer;
+	[Export] private NodePath _pathToSearchContainer;
+	[Export] private NodePath _pathToSearchOptions;
+	[Export] private Array<NodePath> _pathToCategories;
 
-    private void OnSearchTextChanged(string newText)
-    {
-        string newTextLower = newText.ToLower();
-        Node searchContainer = GetNode<Node>(_pathToSearchContainer);
-        if (searchContainer == null || searchContainer.GetChildCount() <= 0) return;
-        foreach (Node child in searchContainer.GetChildren())
-        {
-            // Achtung! Durchsucht im Moment nur Name (erstes Feld). - Für Erweiterungen in der Zukunft, benutze statt GetChild(0) lieber GetNode("LabelName"). LabelName kann durch eine Suchoption ersetzt werden.
-            if (child.GetNode("Margin/Box").GetChild(0) is Label textLabel && child is Control control)
-            {
-                control.SetVisible(textLabel.GetText().ToLower().Contains(newTextLower));
-            }
-        }
-    }
+	private SearchOption _searchOption = SearchOption.NAME;
+	
+	private void OnSearchTextChanged(string newText)
+	{
+		string newTextLower = newText.ToLower();
+		Node searchContainer = GetNode<Node>(_pathToSearchContainer);
+		if (searchContainer == null || searchContainer.GetChildCount() <= 0) return;
+
+
+		foreach (Node child in searchContainer.GetChildren())
+		{
+			if (child.GetNode("Margin/Box").GetChild(GetDisplayedCategoryIndex()) is Label textLabel &&
+				child is Control control)
+			{
+				control.SetVisible(textLabel.GetText().ToLower().Contains(newTextLower));
+			}
+		}
+	}
+
+	private void OnSearchOptionItemSelected(int selectedIndex)
+	{
+		OptionButton dropdownMenu = GetNode<OptionButton>(_pathToSearchOptions);
+		string searchOptionString = dropdownMenu.GetItemText(selectedIndex).ToUpper();
+		if (TryParse(searchOptionString, out SearchOption temp))
+		{
+			_searchOption = temp;
+		}
+	}
+
+	private int GetDisplayedCategoryIndex()
+	{
+		Node categories = GetNode<Node>(_pathToCategories);
+		foreach (Node child in categories.GetChildren())
+		{
+		}
+	}
 }
---
+++ scripts/general/simple-objects/SearchOption.cs
@@ -0,0 +1,7 @@
+namespace Goetterblick.scripts.general.simple_objects;
+
+public enum SearchOption
+{
+    NAME,
+    CATEGORY
+}
