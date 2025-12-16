c22807410ff1a009e597f7971c130b4d1ddeae76
gi2000 <gi831990@gmail.com>
2025-12-16 15:11:59 +0100

    Added first sqlite db file dsa4-1.db

--- scripts/general/node/SearchBar.cs
+++ scripts/general/node/SearchBar.cs
@@ -13,114 +13,114 @@ namespace Goetterblick.scripts.general.node;
 
 public partial class SearchBar : LineEdit
 {
-    [Export] private Array<NodePath> _pathToElementsContainer;
-    [Export] private Array<NodePath> _pathToCategories;
+	[Export] private Array<NodePath> _pathToElementsContainer;
+	[Export] private Array<NodePath> _pathToCategories;
 
-    private SearchOptionType _searchOptionType;
-    private SortOptionType _sortOptionType;
+	private SearchOptionType _searchOptionType;
+	private SortOptionType _sortOptionType;
 
-    public override void _Ready()
-    {
-        base._Ready();
-        SortElements();
+	public override void _Ready()
+	{
+		base._Ready();
+		SortElements();
 
-        EventBus.Instance.OnSearchOptionItemSelected += OnSearchOptionItemSelected;
-        EventBus.Instance.OnSortOptionItemSelected += OnSortOptionItemSelected;
-    }
+		EventBus.Instance.OnSearchOptionItemSelected += OnSearchOptionItemSelected;
+		EventBus.Instance.OnSortOptionItemSelected += OnSortOptionItemSelected;
+	}
 
-    private void OnSortOptionItemSelected(SortOptionType type)
-    {
-        _sortOptionType = type;
-        SortElements();
-    }
+	private void OnSortOptionItemSelected(SortOptionType type)
+	{
+		_sortOptionType = type;
+		SortElements();
+	}
 
-    private void OnSearchOptionItemSelected(SearchOptionType type)
-    {
-        _searchOptionType = type;
-        OnSearchTextChanged(GetText());
-    }
+	private void OnSearchOptionItemSelected(SearchOptionType type)
+	{
+		_searchOptionType = type;
+		OnSearchTextChanged(GetText());
+	}
 
-    private void OnSearchTextChanged(string newText)
-    {
-        FilterElements(newText);
-        SortElements();
-    }
+	private void OnSearchTextChanged(string newText)
+	{
+		FilterElements(newText);
+		SortElements();
+	}
 
-    private void FilterElements(string newText)
-    {
-        string newTextLower = newText.ToLower();
-        foreach (NodePath path in _pathToElementsContainer)
-        {
-            Node searchContainer = GetNode<Node>(path);
-            if (searchContainer == null || searchContainer.GetChildCount() <= 0) return;
+	private void FilterElements(string newText)
+	{
+		string newTextLower = newText.ToLower();
+		foreach (NodePath path in _pathToElementsContainer)
+		{
+			Node searchContainer = GetNode<Node>(path);
+			if (searchContainer == null || searchContainer.GetChildCount() <= 0) return;
 
-            foreach (Node child in searchContainer.GetChildren())
-            {
-                if (child.GetNode("Margin/Box").GetChild(GetDisplayedCategoryIndex(_searchOptionType)) is Label textLabel &&
-                    child is Control control)
-                {
-                    control.SetVisible(TranslationServer.Translate(textLabel.GetText()).ToString().ToLower()
-                        .Contains(newTextLower, StringComparison.CurrentCultureIgnoreCase));
-                }
-            }
-        }
-    }
+			foreach (Node child in searchContainer.GetChildren())
+			{
+				if (child.GetNode("Margin/Box").GetChild(GetDisplayedCategoryIndex(_searchOptionType)) is Label textLabel &&
+					child is Control control)
+				{
+					control.SetVisible(TranslationServer.Translate(textLabel.GetText()).ToString().ToLower()
+						.Contains(newTextLower, StringComparison.CurrentCultureIgnoreCase));
+				}
+			}
+		}
+	}
 
-    private void SortElements()
-    {
-        foreach (NodePath path in _pathToElementsContainer)
-        {
-            Node searchContainer = GetNode<Node>(path);
-            if (searchContainer == null || searchContainer.GetChildCount() <= 0) return;
+	private void SortElements()
+	{
+		foreach (NodePath path in _pathToElementsContainer)
+		{
+			Node searchContainer = GetNode<Node>(path);
+			if (searchContainer == null || searchContainer.GetChildCount() <= 0) return;
 
-            List<Node> sorted = searchContainer.GetChildren().OrderBy(child => GetTextToSortBy(_sortOptionType, child)).ToList();
-            for (int i = 0; i < sorted.Count && i < searchContainer.GetChildCount(); i++)
-            {
-                searchContainer.MoveChild(sorted[i], i);
-            }
-        }
-    }
+			List<Node> sorted = searchContainer.GetChildren().OrderBy(child => GetTextToSortBy(_sortOptionType, child)).ToList();
+			for (int i = 0; i < sorted.Count && i < searchContainer.GetChildCount(); i++)
+			{
+				searchContainer.MoveChild(sorted[i], i);
+			}
+		}
+	}
 
-    private string GetTextToSortBy(SortOptionType sortBy, Node child)
-    {
-        switch (sortBy)
-        {
-            case SortOptionType.ALPHABETICAL:
-                if (child.GetNode("Margin/Box").GetChild(GetDisplayedCategoryIndex(SearchOptionType.NAME)) is Label textLabel)
-                {
-                    return TranslationServer.Translate(textLabel.Text).ToString();
-                }
+	private string GetTextToSortBy(SortOptionType sortBy, Node child)
+	{
+		switch (sortBy)
+		{
+			case SortOptionType.ALPHABETICAL:
+				if (child.GetNode("Margin/Box").GetChild(GetDisplayedCategoryIndex(SearchOptionType.NAME)) is Label textLabel)
+				{
+					return TranslationServer.Translate(textLabel.Text).ToString();
+				}
 
-                return null;
-            case SortOptionType.CREATIONDATE:
-                return null;
-            default:
-                throw new ArgumentOutOfRangeException(nameof(sortBy), sortBy, null);
-        }
-    }
+				return null;
+			case SortOptionType.CREATIONDATE:
+				return null;
+			default:
+				throw new ArgumentOutOfRangeException(nameof(sortBy), sortBy, null);
+		}
+	}
 
-    /// <summary>
-    /// Gets the Index of the currently selected search option.
-    /// </summary>
-    /// <returns>Returns either the index or -1 if no category of this name was found.</returns>
-    private int GetDisplayedCategoryIndex(SearchOptionType searchOptionType)
-    {
-        foreach (NodePath path in _pathToCategories)
-        {
-            Node categories = GetNode<Node>(path);
-            for (int i = 0; i < categories.GetChildCount(); i++)
-            {
-                Node child = categories.GetChild(i);
+	/// <summary>
+	/// Gets the Index of the currently selected search option.
+	/// </summary>
+	/// <returns>Returns either the index or -1 if no category of this name was found.</returns>
+	private int GetDisplayedCategoryIndex(SearchOptionType searchOptionType)
+	{
+		foreach (NodePath path in _pathToCategories)
+		{
+			Node categories = GetNode<Node>(path);
+			for (int i = 0; i < categories.GetChildCount(); i++)
+			{
+				Node child = categories.GetChild(i);
 
-                if (child is not Label textLabel) continue;
-                string categoryText = textLabel.GetText().ToUpper();
-                if (categoryText.EndsWith(searchOptionType.ToString()))
-                {
-                    return i;
-                }
-            }
-        }
+				if (child is not Label textLabel) continue;
+				string categoryText = textLabel.GetText().ToUpper();
+				if (categoryText.EndsWith(searchOptionType.ToString()))
+				{
+					return i;
+				}
+			}
+		}
 
-        return -1;
-    }
+		return -1;
+	}
 }
